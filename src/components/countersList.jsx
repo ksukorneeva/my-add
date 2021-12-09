d37b35608e3d36d1ba44ec7e26bbf188092a8440
Ксения Корнеева <user@MacBook-Pro-User.local>
2021-12-09 22:21:17 +0300

    first commit

---
+++ src/components/countersList.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import Counter from "./counter"
+
+const CountersList=()=>{
+    const initialState=[
+        {id:0, value:0, name:"Ненужная вещь", price: 200},
+        {id:1, value:0, name:"Ложка"},
+        {id:2, value:0, name:"Вилка"},
+        {id:3, value:0, name:"Тарелка"},
+        {id:4, value:0, name:"Набор минималиста"},
+    ]
+
+    const [counters, setCounters] = useState(initialState)
+const handleDelete=(id)=>{
+    const newCounters=counters.filter(c=>c.id!==id)
+    setCounters(newCounters)
+}
+const handleReset=()=>{
+    setCounters(initialState)
+    console.log('handle reset')
+}
+const handleIncrement=(id)=>{
+    counters[id].value = counters[id].value+1
+    const newCounter = counters.map(c=>c)
+    setCounters(newCounter)
+}
+const handleDecrement=(id)=>{
+    counters[id].value=counters[id].value-1
+    const newCounter = counters.map(c=>c)
+    setCounters(newCounter)
+}
+return <>
+    {counters.map(count=>(
+        <Counter 
+        key={count.id} 
+        onDelete={handleDelete}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+        {...count}
+        />
+    ))}
+    <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>
+</>
+}
+export default CountersList
